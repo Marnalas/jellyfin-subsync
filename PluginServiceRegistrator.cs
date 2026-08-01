f3bd87cf76d59fdbf93be00f21bed5efca6fd3c1
jeanchristophe.mqt@gmail.com <jeanchristophe.mqt@gmail.com>
2026-08-01 16:12:55 +0200

    feat: rename data folder

---
+++ PluginServiceRegistrator.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using Jellyfin.Subsync.Starter.Infrastructure;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Controller;
+using MediaBrowser.Controller.Plugins;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Jellyfin.Subsync.Starter
+{
+    /// <summary>
+    /// Located by Jellyfin at server startup and used to register services
+    /// that need constructor arguments Jellyfin's DI container can't invent
+    /// on its own (here, SkipCache's on-disk data folder).
+    /// </summary>
+    public class PluginServiceRegistrator : IPluginServiceRegistrator
+    {
+        public void RegisterServices(IServiceCollection serviceCollection, IServerApplicationHost applicationHost)
+        {
+            serviceCollection.AddSingleton(provider =>
+            {
+                var applicationPaths = provider.GetRequiredService<IApplicationPaths>();
+                var dataFolder = Path.Combine(applicationPaths.DataPath, "subsync-starter");
+                var logger = provider.GetRequiredService<ILogger<SkipCache>>();
+                return new SkipCache(dataFolder, logger);
+            });
+        }
+    }
+}
